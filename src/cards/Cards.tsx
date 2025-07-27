e65d5f8cc57add7b8c65ebbfe44af8aa634520e6
eliashet <eliashet@stud.ntnu.no>
2025-07-27 17:42:51 +0200

    save as folder

--- src/cards/Cards.tsx
+++ src/cards/Cards.tsx
@@ -2,6 +2,8 @@ import { Card } from "../card/Card";
 import type { IOptions } from "../editor/Editor";
 import { numberOfPhotosNeeded } from "../logic/generateMono";
 import styles from "./Cards.module.css";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
 
 interface ICardsProps {
   cards: number[][];
@@ -10,21 +12,25 @@ interface ICardsProps {
 }
 
 export const Cards = (props: ICardsProps) => {
-  const saveCanvas = () => {
+  const saveCanvas = async () => {
+    const canvases = document.querySelectorAll("canvas");
+    const zip = new JSZip();
+    const folder = zip.folder("falkeblikk");
 
-const canvases = document.querySelectorAll("canvas");
+    for (let i = 0; i < canvases.length; i++) {
+      const canvas = canvases[i] as HTMLCanvasElement;
+      const dataUrl = canvas.toDataURL("image/png");
+      const base64 = dataUrl.split(",")[1]; // remove the header
 
-canvases.forEach((canvas, index) => {
-  const link = document.createElement("a");
-  link.download = `falkeblikk_${index + 1}.png`; // Unique filename using index
-  link.href = canvas.toDataURL();
-  link.click();
-});
+      folder?.file(`falkeblikk_${i + 1}.png`, base64, { base64: true });
+    }
 
+    const content = await zip.generateAsync({ type: "blob" });
+    saveAs(content, "falkeblikk.zip");
   };
 
   return (
-  <div className={styles.container}>
+    <div className={styles.container}>
       <button
         className={styles.downloadButton}
         type="button"
@@ -40,16 +46,16 @@ canvases.forEach((canvas, index) => {
         </p>
       )}
 
-    <div className={styles.scrollContainer}>
-      {props.cards.map((card, index) => (
-        <Card
-        key={index}
-        card={card}
-        images={props.images}
-        options={props.options}
-        />
-      ))}
+      <div className={styles.scrollContainer}>
+        {props.cards.map((card, index) => (
+          <Card
+            key={index}
+            card={card}
+            images={props.images}
+            options={props.options}
+          />
+        ))}
+      </div>
     </div>
-        </div>
   );
 };
